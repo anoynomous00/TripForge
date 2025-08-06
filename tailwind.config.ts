05564bd49f53828d4f05b73eb9ec7eeba59ab145
Ankush Pateel <ankushpateel6@gmail.com>
2025-08-06 08:46:46 +0000

    add effect to text tripforge in front page up to 2 sec effect add

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -112,7 +112,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
+        'fade-in-up': 'fade-in-up 2s ease-out forwards',
         'long-fade-in': 'long-fade-in 1.5s ease-out forwards',
       },
     },
